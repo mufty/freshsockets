ea46e6bdef0cade1553c6a9446e4991157913b73
Michal Gubriansky <gubriansky@sernato.sk>
2013-09-26 11:06:48 +0200

    added requirejs initial implementation

--- framework/client/src/core.js
+++ framework/client/src/core.js
@@ -1,3 +1,42 @@
-window.onload = function(){
+require(['DOM/traversal'], function(){
+	var ws = new WebSocket("ws://localhost:8080/WonderfulSockets/websocket/test");
+	ws.onmessage= function(data) {
+		var json = JSON.parse(data.data);
+		if(json.modelBinding){
+			var a = document.getElementsByAttr("ws-model="+json.modelBinding);
+			for(var i in a){
+				var elm = a[i];
+				elm.val(json.modelValue);
+			}
+		} else if(json.name){
+			document.getElementById('name').val(json.name);
+			document.getElementById('surname').val(json.surname);
+			document.getElementById('message').val(json.message);
+		}
+	};
 	
-};
+	function sendChange(e){
+		var elm = e.currentTarget;
+		var wsModel = elm.attr("ws-model");
+		var wsValue;
+		if(elm.val)
+			wsValue = elm.val();
+		ws.send('{"modelBinding":"'+wsModel+'","modelValue":"'+wsValue+'", "action":"update"}');
+	}
+	
+	function init(){
+		var a = document.getElementsByAttr("ws-model");
+		for(var i in a){
+			var elm = a[i];
+			elm.onchange = sendChange;
+		};
+	}
+	
+	if(document.readyState === "complete"){
+		init();
+	} else {
+		window.onload = function(){
+			init();
+		};
+	}
+});
